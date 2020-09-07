511b84f3c1408346d11b99c57d2f89c11ffb37f4
ssw <250853210@qq.com>
2020-09-07 22:16:50 +0800

    Initial Commit

---
+++ IEMS 5710/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IEMS 5710
+//
+//  Created by 某某 s on 2020/9/7.
+//  Copyright © 2020 某某 s. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
